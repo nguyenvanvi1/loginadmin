a0d2a28052dfe494d306244ad59438b494054e44
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2022-03-01 00:09:12 +0700

    upload file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import LoginForm from './component/LoginForm';
+
 
 function App() {
+  const adminUser = {
+    email:"admin@admin.com",
+    password:"admin123"
+  }
+  const [user,setUser] = useState({name:"",email:""})
+  const [error,setError] = useState("")
+  const Login = details=>{
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("thanh cong")
+      setUser({
+        name:details.name,
+        email: details.email
+      })
+    }else{
+      setError("Vui lòng nhập đầy đủ các trường")
+    }
+  }
+  const Logout = ()=>{
+    console.log("Logout")
+    setUser({name: "", email:""})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+      <div className='welcome'>
+        <h2>Welcome<span> {user.name}</span></h2>
+        <h1>{user.email}</h1>
+        <button onClick={Logout}>Logout</button>
+      </div>):(
+        <LoginForm Login={Login} error={error}/>
+      )}
     </div>
   );
 }
